c4e78430bbf0592d05c913f14de404671dcf9c23
positivehun <jihun3104@gmail.com>
2022-12-03 21:15:43 +0900

    221203

---
+++ HashMapSample.java
@@ -0,0 +1,14 @@
+import java.util.HashMap;
+
+public class HashMapSample {
+    public static void main(String[] args) {
+        HashMap<Integer,String> score = new HashMap<>();
+        score.put(3,"Korea");
+        score.put(4,"Porto");
+
+
+        System.out.println(score);
+
+
+    }
+}
---
+++ LambdaFunctionSample.java
@@ -0,0 +1,6 @@
+public class LambdaFunctionSample {
+    public static void main(String[] args) {
+        //
+
+    }
+}
